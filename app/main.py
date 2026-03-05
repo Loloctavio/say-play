214c50c25a3c4d8deb4190008c59dad75c3924a2
Loloctavio <octavio.fonseca@derevo.com.mx>
2026-03-05 11:19:46 -0600

    added CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.playlists_routes import router as playlists_router
 from app.routes.user_routes import router as users_router
 from app.db.mongo import ping, close_mongo
 
 app = FastAPI(title="AI Playlist API", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router)
 app.include_router(playlists_router)
